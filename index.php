5de928a96795050e26652af21c0c83cf691f3fe6
AndreaSdr70 <andrea@sdringola.it>
2024-08-08 21:34:12 +0200

    Selfwork PHP 1

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+
+$int = 15;
+var_dump ($int);
+
+$float = 12.50;
+var_dump ($float);
+
+$string = "Ciao a tutti";
+var_dump ($string);
+
+$boolean = true;
+var_dump ($boolean);
+
+const v1 = '$int';
+var_dump (v1);
+
+const v2 = '$float';
+var_dump (v2);
+
+const v3 = '$string';
+var_dump (v3);
+
+const v4 = '$boolean';
+var_dump (v4);
+
+?>
